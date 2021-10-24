f4dd15d6ef3d7a496943d54ab12af560b8701225
jossrc <kuro.rc10@gmail.com>
2021-10-24 16:44:21 -0500

    Manejo de rutas con controladores

--- routes/flight.js
+++ routes/flight.js
@@ -2,12 +2,13 @@ const { Router } = require('express');
 
 const router = Router();
 
+// Controladores
+const { getFlights, getFlight } = require('../controllers/flight');
+
 // Listado de vuelos
-router.get('/', (request, response) => {
-  response.json({
-    ok: true,
-    message: 'Listado de vuelos',
-  });
-});
+router.get('/', getFlights);
+
+// Obtener un vuelo
+router.get('/:id', getFlight);
 
 module.exports = router;
