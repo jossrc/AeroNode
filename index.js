bdf41f2617c42b2ca1b1176bbd9126348d54af18
jossrc <kuro.rc10@gmail.com>
2021-10-24 15:54:38 -0500

    Manejo de las rutas globales

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 
 const app = express();
-
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!',
-  })
-});
+// Manejando globalmente las rutas
+app.use(require('./routes/index'));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
