8d4efea79bd8a87ec4ffb985816cbab14f1b7aa8
jossrc <kuro.rc10@gmail.com>
2021-10-24 18:16:33 -0500

    Lectura y parse del body

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Lectura y parseo del body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Manejando globalmente las rutas
 app.use(require('./routes/index'));
 
