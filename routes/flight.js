645abd75d24022aeda8204634fb2773cb98c2934
jossrc <kuro.rc10@gmail.com>
2021-10-24 17:03:26 -0500

    ruta para el registro del vuelo

--- routes/flight.js
+++ routes/flight.js
@@ -3,12 +3,15 @@ const { Router } = require('express');
 const router = Router();
 
 // Controladores
-const { getFlights, getFlight } = require('../controllers/flight');
+const { getFlights, getFlight, createFlight } = require('../controllers/flight');
 
 // Listado de vuelos
 router.get('/', getFlights);
 
 // Obtener un vuelo
 router.get('/:id', getFlight);
+
+// Registrar un vuelo
+router.post('/', createFlight);
 
 module.exports = router;
